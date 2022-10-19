a18ecf33b174f53c03ad5b158cf3e20e47fae943
Kosawat Sukchaya <kosawat@Kosawats-MacBook-Air-M1.local>
2022-10-19 17:10:49 +0200

    Added dotenv package

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import helmet from "helmet";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 const app = express();
