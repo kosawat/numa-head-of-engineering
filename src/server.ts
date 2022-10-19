9fb452fc197afc479a2f62fd43cc19d8fee2bbed
Kosawat Sukchaya <kosawat@Kosawats-MacBook-Air-M1.local>
2022-10-19 16:22:49 +0200

    Initialized Express app

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+const server = app.listen(PORT, () => {
+  console.log(`Server listening at port ${PORT}`);
+});
