99ece865e7d5a168689adaabf101e31ae4e5b289
Kosawat Sukchaya <kosawat@Kosawats-MacBook-Air-M1.local>
2022-10-20 11:54:26 +0200

    Added reservation model

---
+++ src/models/reservation.model.ts
@@ -0,0 +1,117 @@
+import mongoose from "mongoose";
+
+const bookingSchema = new mongoose.Schema({
+  id: {
+    type: String,
+  },
+});
+
+const personSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+});
+
+const billingAddressSchema = new mongoose.Schema({
+  companyName: {
+    type: String,
+  },
+  addressLine1: {
+    type: String,
+  },
+  addressLine2: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  postalCode: {
+    type: String,
+  },
+  countryCode: {
+    type: String,
+  },
+  regionCode: {
+    type: String,
+  },
+  taxId: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+});
+
+const reservationSchema = new mongoose.Schema(
+  {
+    id: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    booking: {
+      type: bookingSchema,
+    },
+    propertyId: {
+      type: String,
+      required: true,
+    },
+    pmsId: {
+      type: String,
+      required: true,
+    },
+    arrival: {
+      type: Date,
+      required: true,
+    },
+    departure: {
+      type: Date,
+      required: true,
+    },
+    adults: {
+      type: Number,
+      required: true,
+    },
+    primaryGuest: {
+      type: personSchema,
+      required: true,
+    },
+    additionalGuests: {
+      type: [personSchema],
+      required: true,
+    },
+    booker: {
+      type: personSchema,
+      required: true,
+    },
+    travelPurpose: {
+      type: String,
+      enum: ["Business", "Leisure"],
+    },
+    requestEInvoice: {
+      type: Boolean,
+    },
+    billingAddress: {
+      type: billingAddressSchema,
+    },
+    recipients: {
+      type: [String],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const ReservationModel = mongoose.model("Reservation", reservationSchema);
+
+export default ReservationModel;
