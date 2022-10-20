753af15473ad71628d183fa382fd426f71c77247
Kosawat Sukchaya <kosawat@Kosawats-MacBook-Air-M1.local>
2022-10-20 10:26:25 +0200

    Initialized reservations api routes

--- src/routes.ts
+++ src/routes.ts
@@ -6,10 +6,32 @@ function routes(app: Express) {
     res.status(200).json({ message: "API is Running" })
   );
 
-  // Health check
-  app.get("/healthcheck", (req: Request, res: Response) => {
-    res.sendStatus(200);
+  // Create a new reservation
+  app.post("/api/reservations", (req: Request, res: Response) => {
+    console.log(`POST a reservation: ${JSON.stringify(req.body)}`);
+    res.send("POST /api/reservations is called");
   });
+
+  // Get all reservations
+  app.get("/api/reservations", (req: Request, res: Response) => {
+    console.log(`GET all reservations`);
+    res.send("GET /api/reservations is called");
+  });
+
+  // Get a reservation by ID
+  app.get("/api/reservations/:reservationId", (req: Request, res: Response) => {
+    console.log(`GET reservation ID: ${req.params.reservationId}`);
+    res.send("GET /api/reservations/:reservationId is called");
+  });
+
+  // Update a reservation by ID
+  app.patch(
+    "/api/reservations/:reservationId",
+    (req: Request, res: Response) => {
+      console.log(`Update reservation ID: ${req.params.reservationId}`);
+      res.send("PATCH /api/reservations/:reservationId is called");
+    }
+  );
 }
 
 export default routes;
